246f92ab87a135dfe4e599d4c87fb52c03e87920
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-13 23:38:36 +0200

    Added menu items

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
-const menuIcon = document.querySelector('.menu');
-const navbar = document.querySelector('#myLinks');
+const toggleMenu = document.querySelector('.menu');
+const closeBtn = document.querySelector('#close-btn');
+const menuItems = document.querySelectorAll(".menu-item")
+//for-top-nav
+function openNav() {
+    document.querySelector('.mobile-menu').classList.add('active');
+    toggleMenu.classList.add('invisible');
+} 
 
-menuIcon.onclick = ()=>  {
-  menuIcon.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-  console.log("Clicked")
-};
+toggleMenu.addEventListener('click', openNav)
 
-window.onscroll = () => {
-  menuIcon.classList.remove('fa-times');
+function closeNav(){
+    document.querySelector('.mobile-menu').classList.remove('active');
+    toggleMenu.classList.remove('invisible');
+    document.body.style.overflow = 'auto';
+} 
 
-  navbar.classList.remove('active');
-};
+closeBtn.addEventListener('click', closeNav)
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', closeNav)
+});
+
+
+//for scrolling effect //
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//for highlighting nav-menu//
+
+const sections = document.querySelectorAll('#Headline')
+const navLi = document.querySelectorAll('nav ul li')
+
+window.addEventListener('scroll',() =>{
+    let currentSection = '';
+
+    sections.forEach(section =>{
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop-sectionHeight / 3.5)){
+            currentSection = section.getAttribute('id')
+        }
+    })
+    navLi.forEach(list=>{
+        list.classList.remove('active')
+        if(list.classList.contains(currentSection)){
+          list.classList.add('active')
+       }
+    })
+})

61e0674807b38121b87aa2cff8c3cfec97ff3460
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-14 12:37:03 +0200

    javascript errors correcting

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const toggleMenu = document.querySelector(".menu");
-const closeBtn = document.querySelector("#close-btn");
-const menuItems = document.querySelectorAll(".menu-item");
-//for-top-nav
+const toggleMenu = document.querySelector('.menu');
+const closeBtn = document.querySelector('#close-btn');
+const menuItems = document.querySelectorAll('.menu-item');
 function openNav() {
-  document.querySelector(".mobile-menu").classList.add("active");
-  toggleMenu.classList.add("invisible");
-  document.querySelector(".menu-item").classList.add("open-menu");
+  document.querySelector('.mobile-menu').classList.add('active');
+  toggleMenu.classList.add('invisible');
+  document.querySelector('.menu-item').classList.add('open-menu');
 }
 
-toggleMenu.addEventListener("click", openNav);
+toggleMenu.addEventListener('click', openNav);
 
 function closeNav() {
-  document.querySelector(".mobile-menu").classList.remove("active");
-  toggleMenu.classList.remove("invisible");
-  document.body.style.overflow = "auto";
-  document.querySelector(".menu-item").classList.remove("open-menu");
+  document.querySelector('.mobile-menu').classList.remove('active');
+  toggleMenu.classList.remove('invisible');
+  document.body.style.overflow = 'auto';
+  document.querySelector('.menu-item').classList.remove('open-menu');
 }
 
-closeBtn.addEventListener("click", closeNav);
+closeBtn.addEventListener('click', closeNav);
 
 menuItems.forEach((item) => {
-  item.addEventListener("click", closeNav);
+  item.addEventListener('click', closeNav);
 });
 
-//for scrolling effect //
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
   });
 });

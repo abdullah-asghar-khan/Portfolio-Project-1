1f3bc47d4ea9e4e2b8671ecb52d9474745109ac7
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-15 09:30:56 +0200

    Changes required done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const toggleMenu = document.querySelector('.menu');
 const closeBtn = document.querySelector('#close-btn');
 const menuItems = document.querySelectorAll('.menu-item');
+const home = document.querySelector('.homepage');
+const body = document.querySelector('body');
 function openNav() {
   document.querySelector('.mobile-menu').classList.add('active');
   toggleMenu.classList.add('invisible');
   document.querySelector('.menu-item').classList.add('open-menu');
+  body.style.position = 'fixed';
 }
 
 toggleMenu.addEventListener('click', openNav);
@@ -13,20 +16,13 @@ function closeNav() {
   document.querySelector('.mobile-menu').classList.remove('active');
   toggleMenu.classList.remove('invisible');
   document.body.style.overflow = 'auto';
+  body.style.position = 'absolute';
   document.querySelector('.menu-item').classList.remove('open-menu');
+  
 }
 
 closeBtn.addEventListener('click', closeNav);
 
 menuItems.forEach((item) => {
   item.addEventListener('click', closeNav);
-});
-
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-    });
-  });
 });

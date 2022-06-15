64729495e333003f69ee7f49763e0a48cc9578f4
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-15 16:50:15 +0200

    Everything deleted and added again

--- pop-up.js
+++ pop-up.js
@@ -1,183 +1,192 @@
-const projects = [
-  {
-    name: 'Tonic',
-    devInfo: 'Abdullah',
-    description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ['html', 'css', 'javascript'],
-    image: '(./images/Card-works.png)',
-    liveVersion: 'See Live',
-    sourceLink: 'See Source',
-  },
+const projects = {
+    p15: {
+      name: 'Multi-Post Stories',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys ',
+      img: './images/Img-Placeholder.png',
+      technolgies: ['css', 'HTML', 'bootstap', 'Ruby'],
+      linkToLiveVersion: '#',
+      linkToSource: 'https://github.com/Abdullah2213565/Portfolio-Project-1/',
+      generalInf: ['Microverse- Student', 'Web Developer', 2021],
+    },
+    p16: {
+      name: 'Shiz Pet Store',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+      img: './images/Card-works.png',
+      technolgies: ['css', 'HTML', 'bootstap', 'Ruby'],
+      linkToLiveVersion: '#',
+      linkToSource: 'https://github.com/Abdullah2213565/Portfolio-Project-1/',
+      generalInf: ['Microverse- Student', 'Web Developer', 2021],
+    },
+    p17: {
+      name: 'Movie Engine',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+      img: './images/Card-works.png',
+      technolgies: ['css', 'HTML', 'bootstap', 'Ruby'],
+      linkToLiveVersion: '#',
+      linkToSource: 'https://github.com/Abdullah2213565/Portfolio-Project-1/',
+      generalInf: ['Microverse- Student', 'Web Developer', 2021],
+    },
+  };
 
-  {
-    name: 'Multi-Post Stories',
-    devInfo: 'Abdullah',
-    description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ['html', 'css', 'javascript', 'React'],
-    image: '(./images/Card-works.png)',
-    liveVersion: 'See Live',
-    sourceLink: 'See Source',
-  },
+const myWork = document.querySelector('#recent-works');
 
-  {
-    name: 'Tonic',
-    devInfo: 'Abdullah',
-    description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ['html', 'css', 'javascript'],
-    image: '(./images/Card-works.png)',
-    liveVersion: 'See Live',
-    sourceLink: 'See Source',
-  },
+function createMobileProjectCard(key) {
+  const projectSection = document.createElement('div');
+  const img = document.createElement('img');
+  const general = document.createElement('div');
+  const infGeneral = document.createElement('div');
+  const title = document.createElement('h3');
+  const inf = document.createElement('p');
+  const description = document.createElement('p');
+  const techns = document.createElement('ul');
+  const btnLoad = document.createElement('a');
+  const footer = document.createElement('footer');
+  const externalLink = document.createElement('div');
+  const descriptionList = projects[key].description.split(' ');
+  const descriptionNbrWords = descriptionList.length;
+  externalLink.appendChild(btnLoad);
+  externalLink.classList.add('external_link');
+  footer.appendChild(techns);
+  footer.appendChild(externalLink);
+  general.appendChild(title);
+  general.appendChild(infGeneral);
+  general.appendChild(description);
+  general.appendChild(footer);
+  general.classList.add('general');
+  infGeneral.appendChild(inf);
+  infGeneral.id = 'general_inf';
+  projectSection.appendChild(img);
+  img.src = projects[key].img;
+  img.alt = 'Image project';
+  img.classList.add('project_image');
+  projectSection.appendChild(general);
+  title.innerHTML = projects[key].name;
+  inf.innerHTML = `${projects[key].generalInf[0]}  &bullet;  ${projects[key].generalInf[1]}  &bullet;  ${projects[key].generalInf[2]}`;
+  inf.classList.add('title');
+  description.innerHTML = descriptionList
+    .slice(0, Math.min(17, Math.floor(0.4 * descriptionNbrWords)))
+    .join(' ');
+  description.innerHTML += '...';
 
-  {
-    name: 'Multi-Post Stories',
-    devInfo: 'Abdullah',
-    description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ['html', 'css', 'javascript'],
-    image: '(./images/Card-works.png)',
-    liveVersion: 'See Live',
-    sourceLink: 'See Source',
-  },
-];
+  description.classList.add('project_description');
 
-const recentWorks = document.getElementById('#recent-works');
-function addProjects(n) {
-  const {
-    name,
-    devInfo,
-    description,
-    languages,
-    image,
-  } = projects[n];
+  projects[key].technolgies.forEach((element) => {
+    techns.innerHTML = `${techns.innerHTML}<li>${element} </li>`;
+  });
+  techns.classList.add('skills');
+  btnLoad.innerHTML = 'See Project';
+  btnLoad.href = '#';
+  projectSection.appendChild(general);
+  projectSection.key = key;
+  projectSection.className = 'project';
 
-  recentWorks.innerHTML += `
-
-    <div class="project">
-<img src=${image} alt="Screenshot-of-*project-name*">
-<section class="project-body">
-    <h3 class="project-name">
-        ${name}
-    </h3>
-    <div class="developer-info">
-        <h4 class="developer-name">
-            ${devInfo}
-        </h4>
-        <ul>
-            <li>● Back End Dev</li>
-            <li>● 2022</li>
-        </ul>
-    </div>
-    <p class="project-description">
-        ${description}
-    </p>
-    <ul class="languages-used">
-        <li class="language"> ${languages[0]}</li>
-        <li class="language">${languages[1]}</li>
-        <li class="language">${languages[2]}</li>
-    </ul>
-    <button  type="button" class="project${n}-link" >
-        See Project
-    </button>
-</section>
-</div>`;
+  return projectSection;
 }
 
-const seeProjectBtn1 = document.querySelector('.project0-link');// get-project btns
-const seeProjectBtn2 = document.querySelector('.project1-link');// get-project btns
-const seeProjectBtn3 = document.querySelector('.project2-link');// get-project btns
-const seeProjectBtn4 = document.querySelector('.project3-link');// get-project btns
-const overlay = document.querySelector('#overlay');// get overlay
-const container = document.querySelector('.pop-up-window');// get the popup-window
+Object.keys(projects).forEach((key) => {
+  myWork.appendChild(createMobileProjectCard(key));
+});
 
-function openPopup(index) { // index for getting the project number
-  const {
-    name,
-    devInfo,
-    description,
-    languages,
-    image,
-    liveVersion,
-    sourceLink,
-  } = projects[index];
+const popUpBtn = document.querySelectorAll('.external_link');
+const btnSource = document.createElement('a');
+const btnLive = document.createElement('a');
+const externalButton = document.createElement('div');
+const wrapper = document.createElement('div');
+const home = document.querySelector('.home-page');
+const about = document.querySelector('#about');
+const head = document.querySelector('#nav');
+const wwork = document.querySelector('#work');
+const cancelBtn = document.createElement('img');
+const imgWrapper = document.createElement('div');
+const header = document.createElement('div');
+const title = document.createElement('h3');
+const infos = document.createElement('p');
+const image = document.createElement('img');
+const description = document.createElement('p');
+const skills = document.createElement('ul');
 
-  container.innerHTML = `
-  <div class="pop-up-header">
-  <a class="popup-close-btn">&times;</a>
-  <h3 class="project-name">
-    ${name}
-  </h3>
-  <div class="developer-info">
-    <h4 class="developer-name">
-        ${devInfo}
-    </h4>
-    <ul>
-        <li>● Back End Dev</li>
-        <li>● 2022</li>
-    </ul>
-  </div>
-</div>
-<div class="pop-up-body">
-  <div class="images">
-  <img src=${image} alt="Screenshot-of-*project-name*" class="big-img">
-  </div>
-  <div class="about-project left">
-  <p class="project-description">
-   ${description}  
-  </p>
-  <div class="right">
-  <ul class="languages-used">
-      <li class="language">${languages[0]}</li>
-      <li class="language">${languages[1]}</li>
-      <li class="language">${languages[2]}</li>
-  </ul><hr>
-  <div class="pop-up-btns">
-  <button  type="button" class="pop-up-btn live-btn" >
-      ${liveVersion} 
-      <i class="bi bi-arrow-up-right-circle-fill"></i>
-  </button>
-  <button  type="button" class="pop-up-btn source-btn" >
-      ${sourceLink}
-      <i class="bi bi-github"></i>
-  </button>
-  </div>
-  </div>
-  </div?
-</div>`;
+imgWrapper.classList.add('pop-header-div');
+cancelBtn.src = 'assets/img/Icon.svg';
+imgWrapper.appendChild(cancelBtn);
 
-  container.classList.add('active');
-  overlay.classList.add('active');
-  document.body.style.overflow = 'hidden';
+btnSource.target = '_blank';
+btnLive.target = '_blank';
 
-  const popupCloseBtn = document.querySelector('.popup-close-btn');
-  popupCloseBtn.addEventListener('click', () => {
-    container.classList.remove('active');
-    overlay.classList.remove('active');
-    document.body.style.overflow = 'auto';
+description.classList.add('project_description');
+
+infos.classList.add('inf');
+
+const bottom = document.createElement('div');
+const mobilePart = document.createElement('div');
+
+title.classList.add('pop-header-h3');
+header.appendChild(title);
+header.appendChild(imgWrapper);
+header.classList.add('pop-header');
+btnSource.innerHTML = '<img src="assets/img/gitbutton.png" alt="Live version"> ';
+btnLive.innerHTML = '<img src="assets/img/livebutton.png" alt="Live version"> ';
+externalButton.appendChild(btnLive);
+externalButton.appendChild(btnSource);
+
+bottom.appendChild(skills);
+bottom.appendChild(externalButton);
+
+mobilePart.appendChild(description);
+mobilePart.appendChild(bottom);
+mobilePart.classList.add('mobile-part-class');
+mobilePart.querySelector('p').classList.add('pop-header-description');
+image.classList.add('project_image');
+image.classList.add('pop-header-img');
+
+wrapper.appendChild(header);
+wrapper.appendChild(infos);
+wrapper.appendChild(image);
+wrapper.appendChild(mobilePart);
+
+wrapper.style.zIndex = '999';
+wrapper.style.position = 'absolute';
+wrapper.style.top = '0';
+wrapper.classList.add('pop-window');
+wrapper.style.visibility = 'hidden';
+
+home.appendChild(wrapper);
+
+popUpBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const parentId = btn.parentElement.parentElement.parentElement.key;
+    title.innerHTML = projects[parentId].name;
+    infos.innerHTML = `${projects[parentId].generalInf[0]}  &bullet;  ${projects[parentId].generalInf[1]}  &bullet;  ${projects[parentId].generalInf[2]}`;
+    image.src = projects[parentId].img;
+    description.innerHTML = projects[parentId].description;
+    skills.innerHTML = '';
+    projects[parentId].technolgies.forEach((element) => {
+      skills.innerHTML = `${skills.innerHTML}<li>${element} </li>`;
+    });
+    btnSource.href = projects[parentId].linkToSource;
+    btnLive.href = projects[parentId].linkToLiveVersion;
+    skills.classList.add('skills');
+    skills.classList.add('techns');
+
+    home.style.backgroundColor = 'rgba(193, 199, 208, 0.7)';
+    about.style.backgroundColor = 'rgba(193, 199, 208, 0.7)';
+    head.style.backgroundColor = 'rgba(193, 199, 208, 0.7)';
+    wwork.style.backgroundColor = 'rgba(193, 199, 208, 0.7)';
+    wrapper.style.visibility = 'visible';
+
+    wrapper.style.position = 'fixed';
+    wrapper.style.overflow = 'scroll';
   });
-
-  overlay.addEventListener('click', () => {
-    container.classList.remove('active');
-    overlay.classList.remove('active');
-    document.body.style.overflow = 'auto';
-  });
-}
-
-seeProjectBtn1.addEventListener('click', () => {
-  openPopup(0);
 });
+const cancel = document.querySelector('.pop-header-div');
 
-seeProjectBtn2.addEventListener('click', () => {
-  openPopup(1);
-});
-
-seeProjectBtn3.addEventListener('click', () => {
-  openPopup(2);
-});
-
-seeProjectBtn4.addEventListener('click', () => {
-  openPopup(3);
+cancel.addEventListener('click', () => {
+  document.querySelector('.pop-window').style.visibility = 'hidden';
+  home.style.backgroundColor = '#fff';
+  about.style.backgroundColor = '#fff';
+  head.style.backgroundColor = '#fff';
+  wwork.style.backgroundColor = '#E5E5E5';
+  wrapper.style.position = 'hidden';
 });

de0f5d9f2d376eb086b489b37f838216a06eed99
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-15 12:24:14 +0200

    #recent-works added

--- pop-up.js
+++ pop-up.js
@@ -5,29 +5,29 @@ const projects = [
       description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
       languages: ['html', 'css', 'javascript'],
-      image: './images/Snapshoot-Portfolio.png',
+      image: '(./images/Card-works.png)',
       liveVersion: 'See Live',
       sourceLink: 'See Source',
     },
   
     {
       name: 'Multi-Post Stories',
-      devInfo: 'Abdullah',
+      devInfo: 'Ishpaul',
       description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
       languages: ['html', 'css', 'javascript', 'React'],
-      image: './images/Snapshoot-Portfolio.png',
+      image: '(./images/Card-works.png)',
       liveVersion: 'See Live',
       sourceLink: 'See Source',
     },
   
     {
       name: 'Tonic',
-      devInfo: 'Abdullah',
+      devInfo: 'IAbdullah',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
       languages: ['html', 'css', 'javascript'],
-      image: './images/Snapshoot-Portfolio.png',
+      image: '(./images/Card-works.png)',
       liveVersion: 'See Live',
       sourceLink: 'See Source',
     },
@@ -38,10 +38,21 @@ const projects = [
       description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
       languages: ['html', 'css', 'javascript'],
-      image: './images/Snapshoot-Portfolio.png',
+      image: '(./images/Card-works.png)',
       liveVersion: 'See Live',
       sourceLink: 'See Source',
     },
   ];
+   
+  const recentWorks = document.getElementById('#recent-works');
+  function addProjects(n) {
+    const {
+      name,
+      devInfo,
+      description,
+      languages,
+      image,
+    } = projects[n];
 
-  
+
+

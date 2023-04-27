f9c3d1c4b720fd78515ffb64f62ec011092b8f5c
Abdullah Asghar <dulakhan024@gmail.com>
2023-04-27 13:09:58 +0200

    Updating portfolio

--- javascript/pop-up.js
+++ javascript/pop-up.js
@@ -2,8 +2,8 @@ const projects = {
   p15: {
     name: 'Awesome Books',
     description:
-        'This project is a simple book list app to know how to use, create and learn projects. It was built with javascript. ',
-    img: './images/awesome-books.png',
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys ',
+    img: './images/Img-Placeholder.png',
     technolgies: ['css', 'HTML', 'bootstap', 'Ruby'],
     linkToLiveVersion: '#',
     linkToSource: 'https://github.com/Abdullah2213565/Portfolio-Project-1/',
